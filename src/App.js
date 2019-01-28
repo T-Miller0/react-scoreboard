747edd9ed445e90d2242c29c1be75b65e72419f7
Tom Miller <thomas.j.c.miller@outlook.com>
2019-01-28 17:00:18 +0000

    state set, handleUserInut  handleRowDelete added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ScoreBoard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.filterText = "";
+    this.state.players = [
+      {
+        id: 1,
+        name: player1,
+        score: 0
+      }
+    ];
+    }
+    handleUserInput(filterText) {
+      this.setState({filterText: filterText});
+    };
+    handleRowDelete(players) {
+      var index = this.state.players.indexOf(product);
+      this.setState(this.state.players);
+    };
+
+  };
+  }
   render() {
     return (
       <div className="App">
